bce4fd31867ece6f15dfec639001df2512754e9a
Diyorbek Fazliddinov <95309632+diyorbekcoder1@users.noreply.github.com>
2023-03-11 09:26:02 +0500

    Limitless

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,10 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\HomeController;
 
 
-Route::get('/', 'HomeController@index')->name('frontend.port');
+Route::get('/', [HomeController::class, 'index'])->name('frontend.port');
 
 Route::group(['middleware' => ['auth:web']], function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('limitless');
